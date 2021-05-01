8a454372e6837421c3c75a7f761ab3d035ec156a
Philip <philip.trauth@gmx.de>
2021-05-01 21:47:41 +0200

    Removes some logs

--- 02_backend/index.ts
+++ 02_backend/index.ts
@@ -3,13 +3,9 @@ import express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
-
 // Routes
 app.use("/api", require("./routes/api"))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`)
 })
