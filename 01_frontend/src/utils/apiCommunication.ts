c8313bcaa667061f0b86a83722f651f7d1933bc0
Philip <philip.trauth@gmx.de>
2021-05-02 11:48:45 +0200

    getRecipes from .then chain to await

--- 01_frontend/src/utils/apiCommunication.ts
+++ 01_frontend/src/utils/apiCommunication.ts
@@ -1,10 +1,11 @@
 import { Recipe } from "./Classes/RecipeCollection"
 
 export async function getRecipes(): Promise<Recipe[]> {
-  return fetch("http://localhost:3000/api/getRecipes", {
+  const result = await fetch("http://localhost:3000/api/getRecipes", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
-  }).then((response) => response.json())
+  })
+  return await result.json()
 }
