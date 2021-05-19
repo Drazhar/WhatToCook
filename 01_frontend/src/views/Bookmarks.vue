be1349bc006e9659fc35bb601ef45a1294f274d4
Philip <philip.trauth@gmx.de>
2021-05-19 15:26:15 +0200

    Minor style change

--- 01_frontend/src/views/Bookmarks.vue
+++ 01_frontend/src/views/Bookmarks.vue
@@ -35,13 +35,10 @@ export default Vue.extend({
 <style scoped>
 #bookmarks {
   position: relative;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+  width: 100%;
+  height: 100%;
   background-color: var(--green-8);
   z-index: 2;
-  padding: 6px;
 }
 
 #menu {
@@ -52,6 +49,7 @@ export default Vue.extend({
 }
 
 .entry {
+  box-shadow: 1px 5px 8px 1px rgba(0, 0, 0, 0.15);
   font-size: 1.1em;
   padding: 6px 3px;
   border-radius: 3px;
